26a2c768d91166cbf3a4c629bcd54041aee9c513
Snapwhiz914 <asilversind@gmail.com>
2024-01-12 13:14:49 -0500

    Simulation support + encoder rollover

--- src/main/java/frc/subsystems/Module.java
+++ src/main/java/frc/subsystems/Module.java
@@ -1,22 +1,18 @@
 package frc.subsystems;
 
 import edu.wpi.first.units.Angle;
-import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.MutableMeasure;
-import edu.wpi.first.units.Time;
-import edu.wpi.first.units.Unit;
-import edu.wpi.first.units.Units;
 import edu.wpi.first.units.Velocity;
 import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import frc.robot.Robot;
 import frc.utils.AbsoluteAnalogEncoder;
 
 import static edu.wpi.first.units.MutableMeasure.mutable;
-import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Second;
@@ -31,8 +27,10 @@ public class Module extends SubsystemBase {
     private SysIdRoutine.Config cfg;
 
     private final SysIdRoutine routine;
-    private double lastMeasure;
+    private double veloLast = 0;
     private double rate = 0;
+    private double encoderLast = 0;
+    private double totalRotations = 0;
     
     // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
     private final MutableMeasure<Voltage> appliedVoltage = mutable(Volts.of(0));
@@ -46,7 +44,6 @@ public class Module extends SubsystemBase {
         cfg = new SysIdRoutine.Config(Velocity.combine(Volts, Second).of(0.25), null, null);
 
         routine = new SysIdRoutine(
-          // Empty config defaults to 1 volt/second ramp rate and 7 volt step voltage.
           cfg,
           new SysIdRoutine.Mechanism(
               // Tell SysId how to send the driving voltage to the motors.
@@ -62,7 +59,9 @@ public class Module extends SubsystemBase {
                     .voltage(
                         appliedVoltage.mut_replace(
                             motor.get() * RobotController.getBatteryVoltage(), Volts))
-                    .angularPosition(angle.mut_replace(encoder.getRotationDegrees()/360, Rotations))
+                    .angularPosition(angle.mut_replace(Robot.isReal() ? //If simulated, apply random "ideal" values (ie. get a solid r for a linear best fit)
+                      getEncoderWithRollOver() :
+                      appliedVoltage.magnitude()*Math.random(), Rotations)) 
                     .angularVelocity(
                         velocity.mut_replace(rate, RotationsPerSecond));
               },
@@ -73,7 +72,27 @@ public class Module extends SubsystemBase {
 
     @Override
     public void periodic() {
-      rate = ((encoder.getRotationDegrees()/360)-lastMeasure)/0.02;
+      double read = getEncoderWithRollOver();
+      if (Robot.isSimulation()) {
+        //In this case, apply random "ideal" values (ie. get a solid r for a linear best fit)
+        read = appliedVoltage.magnitude()*Math.random();
+      }
+      rate = (read-veloLast)/0.02;
+      veloLast = read;
+    }
+
+    //Returns the angle of the encoder with rollover (ie. If it changed from 360 to 0, return 361)
+    private double getEncoderWithRollOver() {
+      double read = encoder.getRotationDegrees()/360;
+      if (encoderLast > 0.5 && read < 0.5) { //Rollover past 360
+        totalRotations++;
+      }
+      if (encoderLast < 0.5 && read > 0.5) { //Rollover past 0
+       totalRotations--;
+      }
+      encoderLast = read;
+      if (totalRotations < 0) read*=-1;
+      return totalRotations+(read);
     }
 
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
